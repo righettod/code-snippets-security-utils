f3fdae5cc8b66fc59ae984df798b1df045ec88b3
drighetto <drighetto@excellium-services.com>
2024-06-09 18:09:22 +0200

    Add fix for #2

--- src/main/java/eu/righettod/ProcessingMode.java
+++ src/main/java/eu/righettod/ProcessingMode.java
@@ -4,5 +4,13 @@ package eu.righettod;
  * Enumeration used by the method <code>SecurityUtils.ensureSerializedObjectIntegrity()</code> to define its working mode.
  */
 public enum ProcessingMode {
-    PROTECT, VALIDATE
+    /**
+     * Protection mode: Add the integrity HMAC to the linked serialized object.
+     */
+    PROTECT,
+
+    /**
+     * Validation of the protection mode: Verify the integrity HMAC against the linked serialized object.
+     */
+    VALIDATE
 }
