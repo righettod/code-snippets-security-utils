4b8ab9351a652ff4216e9f6992daa45d9fb4148b
drighetto <drighetto@excellium-services.com>
2024-10-27 10:39:57 +0100

    Debug UT

--- src/test/java/eu/righettod/TestSecurityUtils.java
+++ src/test/java/eu/righettod/TestSecurityUtils.java
@@ -542,7 +542,7 @@ public class TestSecurityUtils {
                 "%25252525252Fhome%25252525252F%25252525252E%25252525252E%25252525252F%25252525252E%25252525252E%25252525252F%25252525252E%25252525252E%25252525252F%25252525252E%25252525252E%25252525252Fetc%25252525252Fpassword", //URL encoding X6
                 "/home/..\\/..\\/..\\/..\\/etc/password",
                 "/home/..\\\\/..\\/..\\\\/..\\/etc/password",
-                "D:\\test..\\\\\\test"
+                "D:\\test..\\\\..\\test"
         );
         invalidPaths.forEach(p -> {
             assertFalse(SecurityUtils.isPathSafe(p), String.format(templateMsgFalseNegative, p));
